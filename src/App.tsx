7915b2d54bface862d75f4ba3791006cb5ae42b7
PasterIvan <paster-ivan@yandex.by>
2023-05-16 00:56:15 +0300

    fix: homepage

--- src/App.tsx
+++ src/App.tsx
@@ -44,8 +44,8 @@ export const App: React.FC = () => {
           <Route path="search/:id" element={<DescriptionVacancy />} />
           <Route path="search/:*" element={<NotFound />} />
           <Route path="favourites" element={<Favorites />} />
-          <Route path="404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="404" />} />
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
       </div>
     </MantineProvider>
